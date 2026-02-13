faab0cd0d3330d335250b9cdd9725ec1b5215544
The Center for Computational Biomedicine Imaging and Modeling (CBIM) <qd66@scarletmail.rutgers.edu>
2026-02-13 18:22:34 -0500

    update member

--- src/utils/mock-data/members.ts
+++ src/utils/mock-data/members.ts
@@ -13,48 +13,13 @@ export const mockPeople: Person[] = [
   // Faculty
   {
     "id": 1,
-    "name": 'Professor Dimitris Metaxas',
+    "name": 'Board of Governors and Distinguished Professor of CS. Dimitris Metaxas',
     "personalPageUrl": 'https://cs.university.edu/~schen',
     "position": 'Faculty',
     "enrollYear": 2001,
     "profileImage": 'https://deeplearn.irdta.eu/2022su/wp-content/uploads/sites/4/2021/10/Dimitris-N.-Metaxas.jpg',
-    "interest": ''
+    "interest": 'Board Interest including 2D/3D Vision Generative, Medical Imaging, Dynamical Modelling, LLM/VLM'
   },
-
-  // {
-  //   id: '2',
-  //   name: 'Prof. Michael Rodriguez',
-  //   personalPageUrl: 'https://cs.university.edu/~mrodriguez',
-  //   position: 'Faculty',
-  //   enrollYear: 2012,
-  //   profileImage: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg'
-  // },
-  // {
-  //   id: '3',
-  //   name: 'Dr. Emily Watson',
-  //   personalPageUrl: 'https://cs.university.edu/~ewatson',
-  //   position: 'Faculty',
-  //   enrollYear: 2018,
-  //   profileImage: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg'
-  // },
-
-  // Postdoctoral Research Associate
-  // {
-  //   id: '4',
-  //   name: 'Dr. Ahmed Hassan',
-  //   personalPageUrl: 'https://cs.university.edu/~ahassan',
-  //   position: 'Postdoctoral Research Associate',
-  //   enrollYear: 2023,
-  //   profileImage: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg'
-  // },
-  // {
-  //   id: '5',
-  //   name: 'Dr. Lisa Park',
-  //   personalPageUrl: 'https://cs.university.edu/~lpark',
-  //   position: 'Postdoctoral Research Associate',
-  //   enrollYear: 2024,
-  //   profileImage: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg'
-  // },
 
   // PhD Students
   // Import the JSON data
@@ -62,7 +27,7 @@ export const mockPeople: Person[] = [
     "name": "Wenyi Mo",
     "interest": "Diffusion Models, Generative Models, Multimodal, MLLM",
     "profileImage": "/images/members/Wenyi_Mo.jpg",
-    "id": 6,
+    "id": 2,
     "personalPageUrl": "https://scholar.google.com/citations?user=e3s9h8MAAAAJ",
     "enrollYear": 2025,
     "position": "PhD Students"
@@ -71,7 +36,7 @@ export const mockPeople: Person[] = [
     "name": "Yanting Yang",
     "interest": "Embodied AI, LLM, Multimodal",
     "profileImage": "/images/members/Yanting_Yang.jpg",
-    "id": 7,
+    "id": 3,
     "personalPageUrl": "https://scholar.google.com/citations?user=UXRnnz8AAAAJ&hl=en",
     "enrollYear": 2025,
     "position": "PhD Students"
@@ -80,7 +45,7 @@ export const mockPeople: Person[] = [
     "name": "Quyen Tran",
     "interest": "Continual Learning, VLM, LLM",
     "profileImage": "/images/members/Quyen_Tran.jpg",
-    "id": 8,
+    "id": 4,
     "personalPageUrl": "https://scholar.google.com/citations?hl=en&user=ZtuZhrMAAAAJ&view_op=list_works&sortby=pubdate",
     "enrollYear": 2025,
     "position": "PhD Students"
@@ -90,7 +55,7 @@ export const mockPeople: Person[] = [
     "name": "Quang Nguyen",
     "interest": "Multimodal, Generative 3D, Video Generation",
     "profileImage": "/images/members/Quang_Nguyen.jpg",
-    "id": 9,
+    "id": 5,
     "personalPageUrl": "https://scholar.google.com/citations?user=SUuo7U4AAAAJ",
     "enrollYear": 2025,
     "position": "PhD Students"
@@ -100,202 +65,101 @@ export const mockPeople: Person[] = [
     "name": "Quan Dao",
     "interest": "Image/Video Generation, 3D Generative",
     "profileImage": "/images/members/Quan_Dao.jpg",
-    "id": 10,
+    "id": 6,
     "personalPageUrl": "https://scholar.google.com/citations?user=g0RS3_kAAAAJ",
     "enrollYear": 2024,
     "position": "PhD Students"
   },
   {
-    "name": "Di Liu",
-    "interest": "Computer Vision, Vision-language Models, LLMs, Computer Graphics",
-    "profileImage": "/images/members/Di_Liu.jpg",
-    "id": 11,
-    "personalPageUrl": "/",
-    "enrollYear": 2024,
-    "position": "PhD Students"
-  },
-  {
     "name": "Yang Zhou",
-    "interest": "MLLM, VLM, LLM",
+    "interest": "MLLM, Agent, Medical",
     "profileImage": "/images/members/Yang_Zhou.jpg",
-    "id": 12,
-    "personalPageUrl": "/",
-    "enrollYear": 2024,
+    "id": 7,
+    "personalPageUrl": "https://scholar.google.com/citations?hl=en&user=BsQ8IUcAAAAJ&view_op=list_works&sortby=pubdate",
+    "enrollYear": 2023,
     "position": "PhD Students"
   }, 
   {
-    "name": "Song Wen",
-    "interest": "Generative Models, Diffusion Models",
-    "profileImage": "/images/members/Song_Wen.jpg",
-    "id": 13,
-    "personalPageUrl": "/",
-    "enrollYear": 2024,
-    "position": "PhD Students"
-  },
-  {
-    "name": "Ligong Han",
-    "interest": "Diffusion Models, Generative Models, LLM",
-    "profileImage": "/images/members/Ligong_Han.jpg",
-    "id": 14,
-    "personalPageUrl": "/",
-    "enrollYear": 2024,
-    "position": "PhD Students"
-  },
-  {
-    "name": "Zhuowei Li",
-    "interest": "Multimodal, LLM, Continual Learning",
-    "profileImage": "/images/members/Zhuowei_Li.jpg",
-    "id": 15,
-    "personalPageUrl": "/",
-    "enrollYear": 2024,
-    "position": "PhD Students"
-  },
-  {
-    "name": "Shiyu Zhao",
-    "interest": "Multimodal LLM, Open Vocabulary Object Detection, Optical Flow Estimation",
-    "profileImage": "/images/members/Shiyu_Zhao.jpg",
-    "id": 16,
-    "personalPageUrl": "/",
-    "enrollYear": 2024,
-    "position": "PhD Students"
-  },
-  {
     "name": "Can Jin",
-    "interest": "Reinforcement Learning, Efficiency",
+    "interest": "Reinforcement Learning, LLM/Vision Efficiency",
     "profileImage": "/images/members/Can_Jin.jpg",
-    "id": 17,
-    "personalPageUrl": "/",
-    "enrollYear": 2024,
-    "position": "PhD Students"
-  },
-  {
-    "name": "Meng Ye",
-    "interest": "AI for healthcare",
-    "profileImage": "/images/members/Meng_Ye.jpg",
-    "id": 18,
-    "personalPageUrl": "/",
+    "id": 8,
+    "personalPageUrl": "https://scholar.google.com/citations?user=RK-8dz0AAAAJ&hl=en",
     "enrollYear": 2024,
     "position": "PhD Students"
   },
   {
     "name": "Difei Gu",
-    "interest": "Multimodal Reasoning",
+    "interest": "Explainable Medical",
     "profileImage": "/images/members/Difei_Gu.jpg",
-    "id": 19,
-    "personalPageUrl": "/",
-    "enrollYear": 2024,
-    "position": "PhD Students"
-  },
-  {
-    "name": "Zhanfu Yang",
-    "interest": "3D, LLM, Generative Models",
-    "profileImage": "/images/members/Zhanfu_Yang.jpg",
-    "id": 20,
-    "personalPageUrl": "/",
-    "enrollYear": 2024,
+    "id": 9,
+    "personalPageUrl": "https://scholar.google.com/citations?user=nKuyKx0AAAAJ&hl=en",
+    "enrollYear": 2023,
     "position": "PhD Students"
   },
   {
     "name": "Mingyu Zhao",
-    "interest": "Multimodal, LLM",
+    "interest": "ASL",
     "profileImage": "/images/members/Mingyu_Zhao.jpg",
-    "id": 21,
-    "personalPageUrl": "/",
+    "id": 10,
+    "personalPageUrl": "https://scholar.google.com/citations?user=z9_Niy8AAAAJ&hl=en",
     "enrollYear": 2024,
     "position": "PhD Students"
+  },
+  {
+    "name": "Guoning Zhang",
+    "interest": "Explainable Medical",
+    "profileImage": "/images/members/Difei_Gu.jpg",
+    "id": 11,
+    "personalPageUrl": "https://github.com/GuoningZhang",
+    "enrollYear": 2025,
+    "position": "PhD Students"
+  },
+  {
+    "name": "Tunyu Zhang",
+    "interest": "Explainable Medical",
+    "profileImage": "/images/members/Difei_Gu.jpg",
+    "id": 12,
+    "personalPageUrl": "https://scholar.google.com/citations?user=y3st15YAAAAJ&hl=en",
+    "enrollYear": 2025,
+    "position": "PhD Students"
+  },
+  {
+    "name": "Gerasimos Chatzoudis",
+    "interest": "Explainable AI",
+    "profileImage": "/images/members/Difei_Gu.jpg",
+    "id": 13,
+    "personalPageUrl": "https://scholar.google.com/citations?user=9Lsgha0AAAAJ&hl=en",
+    "enrollYear": 2022,
+    "position": "PhD Students"
+  },
+  {
+    "name": "Bingyu Xin",
+    "interest": "Medical Image",
+    "profileImage": "/images/members/Difei_Gu.jpg",
+    "id": 14,
+    "personalPageUrl": "https://scholar.google.com/citations?user=ENG4A1gAAAAJ&hl=en",
+    "enrollYear": 2020,
+    "position": "PhD Students"
+  },
+  {
+    "name": "Bangwei Guo",
+    "interest": "Medical, MRI, Cardiac Imaging",
+    "profileImage": "/images/members/Difei_Gu.jpg",
+    "id": 15,
+    "personalPageUrl": "https://scholar.google.com/citations?user=GBbfGuUAAAAJ&hl=en",
+    "enrollYear": 2022,
+    "position": "PhD Students"
+  },
+  {
+    "name": "Ruosong Ye",
+    "interest": "Multi-LLM Collaboration, RL, Graph",
+    "profileImage": "/images/members/Difei_Gu.jpg",
+    "id": 16,
+    "personalPageUrl": "https://scholar.google.com/citations?user=9Xduz3AAAAAJ&hl=en",
+    "enrollYear": 2022,
+    "position": "PhD Students"
   }
-
-  // MS Students
-  // {
-  //   id: '14',
-  //   name: 'Ryan Murphy',
-  //   personalPageUrl: 'https://cs.university.edu/~rmurphy',
-  //   position: 'MS Students',
-  //   enrollYear: 2023,
-  //   profileImage: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg'
-  // },
-  // {
-  //   id: '15',
-  //   name: 'Jessica Taylor',
-  //   personalPageUrl: 'https://cs.university.edu/~jtaylor',
-  //   position: 'MS Students',
-  //   enrollYear: 2024,
-  //   profileImage: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg'
-  // },
-  // {
-  //   id: '16',
-  //   name: 'Carlos Mendez',
-  //   personalPageUrl: 'https://cs.university.edu/~cmendez',
-  //   position: 'MS Students',
-  //   enrollYear: 2024,
-  //   profileImage: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg'
-  // },
-  // {
-  //   id: '17',
-  //   name: 'Amanda Johnson',
-  //   personalPageUrl: 'https://cs.university.edu/~ajohnson',
-  //   position: 'MS Students',
-  //   enrollYear: 2023,
-  //   profileImage: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg'
-  // },
-  // {
-  //   id: '18',
-  //   name: 'Daniel Chang',
-  //   personalPageUrl: 'https://cs.university.edu/~dchang',
-  //   position: 'MS Students',
-  //   enrollYear: 2024,
-  //   profileImage: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg'
-  // },
-
-  // Alumni
-  // {
-  //   id: '19',
-  //   name: 'Dr. Jennifer Lee',
-  //   personalPageUrl: 'https://linkedin.com/in/jenniferlee',
-  //   position: 'Alumni',
-  //   enrollYear: 2015,
-  //   profileImage: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg'
-  // },
-  // {
-  //   id: '20',
-  //   name: 'Dr. Robert Wilson',
-  //   personalPageUrl: 'https://linkedin.com/in/robertwilson',
-  //   position: 'Alumni',
-  //   enrollYear: 2016,
-  //   profileImage: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg'
-  // },
-  // {
-  //   id: '21',
-  //   name: 'Dr. Nina Patel',
-  //   personalPageUrl: 'https://linkedin.com/in/ninapatel',
-  //   position: 'Alumni',
-  //   enrollYear: 2017,
-  //   profileImage: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg'
-  // },
-  // {
-  //   id: '22',
-  //   name: 'Mark Davis',
-  //   personalPageUrl: 'https://linkedin.com/in/markdavis',
-  //   position: 'Alumni',
-  //   enrollYear: 2018,
-  //   profileImage: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg'
-  // },
-  // {
-  //   id: '23',
-  //   name: 'Dr. Priya Sharma',
-  //   personalPageUrl: 'https://linkedin.com/in/priyasharma',
-  //   position: 'Alumni',
-  //   enrollYear: 2019,
-  //   profileImage: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg'
-  // },
-  // {
-  //   id: '24',
-  //   name: 'Alex Brown',
-  //   personalPageUrl: 'https://linkedin.com/in/alexbrown',
-  //   position: 'Alumni',
-  //   enrollYear: 2020,
-  //   profileImage: 'https://images.pexels.com/photos/35065/homeless-man-color-poverty.jpg'
-  // }
 ];
 
 // Helper function to group people by position
